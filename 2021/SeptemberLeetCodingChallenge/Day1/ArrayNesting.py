e95b581e1e6fe2a718800cf07e3c7d4176cd3759
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-09-01 23:34:23 +0300

    Add draft version for day 1 and day 2

---
+++ 2021/SeptemberLeetCodingChallenge/Day1/ArrayNesting.py
@@ -0,0 +1,104 @@
+
+
+# Array Nesting
+
+'''
+
+Array Nesting
+
+You are given an integer array nums of length n where nums is a permutation of the numbers in the range [0, n - 1].
+
+You should build a set s[k] = {nums[k], nums[nums[k]], nums[nums[nums[k]]], ... } subjected to the following rule:
+
+    The first element in s[k] starts with the selection of the element nums[k] of index = k.
+    The next element in s[k] should be nums[nums[k]], and then nums[nums[nums[k]]], and so on.
+    We stop adding right before a duplicate element occurs in s[k].
+
+Return the longest length of a set s[k].
+
+
+
+Example 1:
+
+Input: nums = [5,4,0,3,1,6,2]
+Output: 4
+Explanation:
+nums[0] = 5, nums[1] = 4, nums[2] = 0, nums[3] = 3, nums[4] = 1, nums[5] = 6, nums[6] = 2.
+One of the longest sets s[k]:
+s[0] = {nums[0], nums[5], nums[6], nums[2]} = {5, 6, 2, 0}
+
+Example 2:
+
+Input: nums = [0,1,2]
+Output: 1
+
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    0 <= nums[i] < nums.length
+    All the values of nums are unique.
+
+
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+class Solution:
+    def arrayNesting(self, nums: List[int]) -> int:
+        setsList = list()
+        nextElement = None
+        for i, num in enumerate(nums):
+            sk = list()
+            while nextElement != num:
+                nextElement = num
+                sk.append()
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.twoSum(nums = [2,7,11,15], target = 9))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3,2,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
