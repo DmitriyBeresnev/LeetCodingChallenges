825c782b9536118a8c45ccd5a277ad6cdad07f19
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-09-02 19:21:08 +0300

    solution from discussion forum

--- 2021/SeptemberLeetCodingChallenge/Day1/ArrayNesting.py
+++ 2021/SeptemberLeetCodingChallenge/Day1/ArrayNesting.py
@@ -61,13 +61,18 @@ from bisect import bisect, bisect_left
 
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
-        setsList = list()
-        nextElement = None
-        for i, num in enumerate(nums):
-            sk = list()
-            while nextElement != num:
-                nextElement = num
-                sk.append()
+        n = len(nums)
+        visited = [False] * n
+        ans = 0
+        for x in nums:
+            cnt = 0
+            while not visited[x]:
+                cnt += 1
+                visited[x] = True
+                x = nums[x]
+            ans = max(ans, cnt)
+
+        return ans
 
 
 if __name__ == "__main__":
