3d1b5cd5c771b411923a3f4aeb848b1b7be03479
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-09-14 21:41:11 +0300

    add solution for LeetCoding Challenge 2021. September. Day 14. Reverse Only Letters

---
+++ 2021/SeptemberLeetCodingChallenge/Day14/ReverseOnlyLetters.py
@@ -0,0 +1,97 @@
+
+
+# Reverse Only Letters
+
+'''
+
+Reverse Only Letters
+
+Given a string s, reverse the string according to the following rules:
+
+    All the characters that are not English letters remain in the same position.
+    All the English letters (lowercase or uppercase) should be reversed.
+
+Return s after reversing it.
+
+
+
+Example 1:
+
+Input: s = "ab-cd"
+Output: "dc-ba"
+
+Example 2:
+
+Input: s = "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+
+Example 3:
+
+Input: s = "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+
+
+
+Constraints:
+
+    1 <= s.length <= 100
+    s consists of characters with ASCII values in the range [33, 122].
+    s does not contain '\"' or '\\'.
+
+
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        stack = [c for c in s if c.isalpha()]
+        return "".join(stack.pop() if c.isalpha() else c for c in s)
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.twoSum(nums = [2,7,11,15], target = 9))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3,2,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
