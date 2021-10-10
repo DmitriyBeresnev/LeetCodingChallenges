465cf0357d949e84afbcfa4fe5e47af3c3c75b32
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-10-10 13:10:35 +0300

    add the solution of the LeetCoding Challenge 2021. October. Day 10. Bitwise AND of Numbers Range

--- 2021/OctoberLeetCodingChallenge/Day10/BitwiseANDofNumbersRange.py
+++ 2021/OctoberLeetCodingChallenge/Day10/BitwiseANDofNumbersRange.py
@@ -1,10 +1,44 @@
 
 
-# LeetCoding Challenge 2021. October. Day 10
+# LeetCoding Challenge 2021. October. Day 10. Bitwise AND of Numbers Range
 
 '''
 
+201. Bitwise AND of Numbers Range
+Medium
 
+Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+
+
+Example 1:
+
+Input: left = 5, right = 7
+Output: 4
+
+Example 2:
+
+Input: left = 0, right = 0
+Output: 0
+
+Example 3:
+
+Input: left = 1, right = 2147483647
+Output: 0
+
+
+
+Constraints:
+
+    0 <= left <= right <= 231 - 1
+
+Accepted
+187,188
+Submissions
+462,148
+
+Related Topics
+Bit Manipulation
 
 '''
 
@@ -23,20 +57,37 @@ from functools import lru_cache
 from bisect import bisect, bisect_left
 
 
+# naive approach
+class Solution2:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        res = left
+        for i, val in enumerate(range(left, right+1)):
+            res &= val
+            # print(val, i)
+        return res
+
+
+# optimized
 class Solution:
-    pass
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        nShifts = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            nShifts += 1
+        return right << nShifts
 
 
 if __name__ == "__main__":
     #
     start = time.perf_counter()
     solution = Solution()
-    print(solution.twoSum(nums = [2,7,11,15], target = 9))
+    print(solution.rangeBitwiseAnd(left=5, right=7))
     end = time.perf_counter()
     print(f"test 1: {end - start:10.6f} sec")
     #
     start2 = time.perf_counter()
-    print(solution.twoSum(nums = [3,2,4], target = 6))
+    print(solution.rangeBitwiseAnd(left = 1, right = 2147483647))
     end2 = time.perf_counter()
     print(f"test 2: {end2 - start2:10.6f} sec")
     #
