3866010b8fe2685573df9e45312a150552d16e78
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-11-02 18:55:59 +0300

    add the solution of the LeetCoding Challenge 2021. October. Day 30. Longest Duplicate Substring

--- 2021/OctoberLeetCodingChallenge/Day30/LongestDuplicateSubstring.py
+++ 2021/OctoberLeetCodingChallenge/Day30/LongestDuplicateSubstring.py
@@ -1,17 +1,52 @@
 
 
-# LeetCoding Challenge 2021. October. Day 30
+# LeetCoding Challenge 2021. October. Day 30. Longest Duplicate Substring
 
 '''
 
+1044. Longest Duplicate Substring
+Hard
 
+Given a string s, consider all duplicated substrings: (contiguous) substrings of s that occur 2 or more times. The occurrences may overlap.
+
+Return any duplicated substring that has the longest possible length. If s does not have a duplicated substring, the answer is "".
+
+
+
+Example 1:
+
+Input: s = "banana"
+Output: "ana"
+
+Example 2:
+
+Input: s = "abcd"
+Output: ""
+
+
+
+Constraints:
+
+    2 <= s.length <= 3 * 104
+    s consists of lowercase English letters.
+
+Accepted
+47,356
+Submissions
+150,039
 
 
 Related Topics
-
+-
 
 Similar Questions
+String, Binary Search, Sliding Window, Rolling Hash, Suffix Array, Hash Function
 
+Hide Hint 1
+Binary search for the length of the answer. (If there's an answer of length 10, then there are answers of length 9, 8, 7, ...)
+
+Hide Hint 2
+To check whether an answer of length K exists, we can use Rabin-Karp 's algorithm.
 
 '''
 
