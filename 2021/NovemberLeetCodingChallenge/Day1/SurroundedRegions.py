309535685c64d465bcae8c8f67ffa0df060260d6
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-11-02 19:11:03 +0300

    add the solution of the LeetCoding Challenge 2021. October. Day 1. Surrounded Regions

---
+++ 2021/NovemberLeetCodingChallenge/Day1/SurroundedRegions.py
@@ -0,0 +1,103 @@
+
+
+# LeetCoding Challenge 2021. October. Day 1. Surrounded Regions
+
+'''
+
+130. Surrounded Regions
+Medium
+
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+
+
+Example 1:
+
+Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+Explanation: Surrounded regions should not be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. Two cells are connected if they are adjacent cells connected horizontally or vertically.
+
+Example 2:
+
+Input: board = [["X"]]
+Output: [["X"]]
+
+
+
+Constraints:
+
+    m == board.length
+    n == board[i].length
+    1 <= m, n <= 200
+    board[i][j] is 'X' or 'O'.
+
+Accepted
+361,216
+Submissions
+1,112,167
+
+
+Related Topics
+Array, Depth-First Search, Breadth-First Search, Union Find, Matrix
+
+Similar Questions
+Number of Islands
+Medium
+Walls and Gates
+Medium
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+class Solution:
+    pass
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.twoSum(nums = [2 ,7 ,11 ,15], target = 9))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,2 ,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
---
+++ 2021/NovemberLeetCodingChallenge/Day3/main.py
@@ -0,0 +1,68 @@
+
+
+# LeetCoding Challenge 2021. October. Day 3.
+
+'''
+
+
+
+
+Related Topics
+
+
+Similar Questions
+
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+class Solution:
+    pass
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.twoSum(nums = [2 ,7 ,11 ,15], target = 9))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,2 ,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
