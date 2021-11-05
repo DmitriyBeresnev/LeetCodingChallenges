fc1815d3f14cc5988b00b269369768e0e2173d02
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-11-05 16:29:03 +0300

    add the solution of the LeetCoding Challenge 2021. October. Day 5. Arranging Coins and some math notes

--- 2021/NovemberLeetCodingChallenge/Day5/ArrangingCoins.py
+++ 2021/NovemberLeetCodingChallenge/Day5/ArrangingCoins.py
@@ -1,17 +1,51 @@
 
 
-# LeetCoding Challenge 2021. October. Day 5.
+# LeetCoding Challenge 2021. October. Day 5. Arranging Coins
 
 '''
 
+441. Arranging Coins
+Easy
 
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+
+
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+
+Example 2:
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+
+
+
+Constraints:
+
+    1 <= n <= 231 - 1
+
+Accepted
+225,106
+Submissions
+513,292
 
 
 Related Topics
-
+Math, Binary Search
 
 Similar Questions
+-
 
+
+[C++/Python] 4 Simple Solutions w/ Explanation | Brute-Force + Binary Search + Math | Beats 100%
+https://leetcode.com/problems/arranging-coins/discuss/1560442/C%2B%2BPython-4-Simple-Solutions-w-Explanation-or-Brute-Force-%2B-Binary-Search-%2B-Math-or-Beats-100
 
 '''
 
@@ -31,7 +65,8 @@ from bisect import bisect, bisect_left
 
 
 class Solution:
-    pass
+    def arrangeCoins(self, n: int) -> int:
+        return int(math.sqrt(2 * n + 0.25) - 0.5)
 
 
 if __name__ == "__main__":
